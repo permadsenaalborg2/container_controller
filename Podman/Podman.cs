19bd0fa889df7eb85567edc58d8845683807c000
pmad@techcollege.dk <pmad@techcollege.dk>
2024-06-30 23:32:33 +0200

    make it compile

---
+++ Podman/Podman.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using System.Text.Json;
+using System;
+public class Podman
+{
+    public static List<Container> GetContainers()
+    {
+        var proc = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "podman",
+                Arguments = " ps -a --format json",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }
+        };
+
+        proc.Start();
+        JsonDocument doc;
+
+        if (proc.StandardOutput.EndOfStream)
+        {
+            Console.WriteLine("Error");
+            return null;
+        }
+        else
+        {
+            var output = proc.StandardOutput.ReadToEnd();
+            doc = JsonDocument.Parse(output);
+        }
+
+        List<Container> containers = [];
+
+        foreach (JsonElement j in doc.RootElement.EnumerateArray())
+        {
+
+            containers.Add(new Container()
+            {
+                ID = j.GetProperty("Id").ToString(),
+                Name = j.GetProperty("Names")[0].ToString(),
+                State = j.GetProperty("State").ToString(),
+            });
+        }
+        return containers;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,45 +1,8 @@
-﻿using System.Diagnostics;
-using System.Text.Json;
-using System;
+﻿using System;
 using TECHCOOL.UI;
-var proc = new Process 
-{
-    StartInfo = new ProcessStartInfo
-    {
-        FileName = "podman",
-        Arguments = " ps -a --format json",
-        UseShellExecute = false,
-        RedirectStandardOutput = true,
-        CreateNoWindow = true
-    }
-};
 
-proc.Start();
-JsonDocument doc;
-
-if (proc.StandardOutput.EndOfStream)
-{
-    Console.WriteLine("Error");
-    return;
-}
-else
-{
-    var output = proc.StandardOutput.ReadToEnd();
-    doc = JsonDocument.Parse(output);  
-} 
-
-List<Container> containers = [];
-
-foreach (JsonElement j in doc.RootElement.EnumerateArray())
-{
-    
-    containers.Add(new Container() 
-    {
-        ID = j.GetProperty("Id").ToString(),
-        Name = j.GetProperty("Names")[0].ToString(),
-        State = j.GetProperty("State").ToString(),
-    });
-}
-
+var containers = Podman.GetContainers();
 Screen.Display(new ContainerListScreen(containers));
+
+Console.Clear();
 
